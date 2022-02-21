a9638a38557b8ec3c3ab9c07bd8d034961fcd21c
Erwin Dirks <e.dirks@kennisnet.nl>
2022-02-21 12:27:45 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# anonlytics-lib
-PHP library for the anonlytics tracker 
+![License](https://img.shields.io/badge/license-MIT-blue?style=flat-square)
+# Anonlytics Lib PHP
+In a world with all GDPR rules, an IP address is now already labeled as personal information and can therefore not just be passed on by your website/application.
+
+That's why we introduce Anonlytics, the server side analytics tool that only works with anonymized data for website analytics.
+
+Entirely in accordance with all the rules of the GDPR. Our servers are all located in Europe and no IP addresses are used anywhere.
+
+This library is part of https://anonlytics.eu, you need a (free) account to use this bundle for your website/application.
+
+## Installation
+Use composer to add the library as dependency for your project
+
+`composer require defixit/anonlytics-lib-php`
+
+
+## Usage
+
+### Setup
+At first, you need to create an account on [Anonlytics.eu](https://anonlytics.eu) to get your `client_token` and `site_token` to connect to our services.
+
+After you created an account on our website you can create a file with the name `anonlytics.yaml` in the `config/packages/` folder, with the following content:
+
+
+#### Send data to our service
+```php
+<?php
+
+require 'vendor/autoload.php';
+
+use DeFixIT\Anonlytics\Tracker;
+
+$tracker = new Tracker($requestArray, $yourClientToken, $yourSiteToken)
+
+$response = $tracker->sendRequestData();
+```
+
+For the `$requestArray` you can use `$_SERVER` in PHP. The response will be `true` or else we're throwing an `AnonlyticsDataNotSendException` exception.
---
+++ src/Tracker.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DeFixIT\Anonlytics;
+
+use DeFixIT\Anonlytics\Exception\AnonlyticsDataNotSendException;
+use function curl_init;
+use function curl_setopt;
+use function curl_exec;
+use function file_get_contents;
+use function json_decode;
+use function json_encode;
+
+class Tracker
+{
+    private const TRACK_HOST = 'https://api.anonlytics.eu/track';
+    private array $server;
+    private string $clientToken;
+    private string $siteToken;
+
+    public function __construct(array $server, string $clientToken, string $siteToken)
+    {
+        $this->server = $server;
+        $this->clientToken = $clientToken;
+        $this->siteToken = $siteToken;
+    }
+
+    /**
+     * @throws AnonlyticsDataNotSendException
+     */
+    public function sendRequestData(): bool
+    {
+        try {
+            $curl = curl_init();
+
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_URL, self::TRACK_HOST);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $this->getHeaders());
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $this->getPostData());
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+            curl_exec($curl);
+        } catch (\Throwable $exception) {
+            throw new AnonlyticsDataNotSendException(
+                'Error while sending data to host: ' . self::TRACK_HOST,
+                0,
+                $exception
+            );
+        }
+
+        return true;
+    }
+
+    private function getHeaders(): array
+    {
+        return [
+            'Content-type: application/json',
+            'anonlytics-client-token: ' . $this->clientToken,
+            'anonlytics-site-token: ' . $this->siteToken,
+        ];
+    }
+
+    private function getPostData(): string
+    {
+        return json_encode(
+            [
+                'server_software' => $this->server['SERVER_SOFTWARE'] ?? null,
+                'server_protocol' => $this->server['SERVER_PROTOCOL'] ?? null,
+                'server_name' => $this->server['SERVER_NAME'] ?? null,
+                'uri' => $this->server['REQUEST_URI'] ?? null,
+                'method' => $this->server['REQUEST_METHOD'] ?? null,
+                'http_user_agent' => $this->server['HTTP_USER_AGENT'] ?? null,
+                'http_accept_language' => $this->server['HTTP_ACCEPT_LANGUAGE'] ?? null,
+                'http_referer' => $this->server['HTTP_REFERER'] ?? null,
+                'country' => $this->getCountry($this->server['REMOTE_ADDR']) ?? null,
+            ]
+        );
+    }
+
+    private function getCountry(string $ip): ?string
+    {
+        return json_decode(
+                file_get_contents('http://www.geoplugin.net/json.gp?ip=' . $ip)
+            )->geoplugin_countryCode ?? null;
+    }
+}
